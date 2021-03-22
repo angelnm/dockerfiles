a1623bac72428766a2090ace0af2c2282da9f8ba
angelnm <angel.nm@hotmail.es>
2021-03-22 12:28:11 +0100

    hot fix

--- nmt-keras/run.sh
+++ nmt-keras/run.sh
@@ -4,4 +4,4 @@ docker container run --rm --gpus all \
 	-v "$(pwd)"/config.py:/opt/nmt-keras/config.py \
 	-v "$(pwd)"/datasets:/opt/nmt-keras/datasets \
 	-v "$(pwd)"/trained_models:/opt/nmt-keras/trained_models \
-	nmt python3 main.py
+	nmt-keras python3 main.py
